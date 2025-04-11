bd044e21b78296a8fd337b4e948b7fedaef70d14
Shubham Verma <vermaprince2004@gmail.com>
2025-04-11 23:55:04 +0530

    electron setup

--- fccProject/vite.config.ts
+++ fccProject/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',                 //changes the default path used in build process
   build: {
     outDir: 'dist-react',     //changed default build directory to stop conflict with electron.
   }
