38e11ceb4e5fb44ac62f9a83c23179b2812d3492
Shubham Verma <vermaprince2004@gmail.com>
2025-04-12 11:56:27 +0530

    implemented vite HMR

---
+++ fccProject/notes.ts
@@ -0,0 +1,15 @@
+// cross-env is being used to set environment variables in windows
+
+/* 
+    Whenever code in electron folder changes, 
+    we need to run the transpile cmd to recompile it in js 
+    and restart the electron app.
+
+    Vite Hot Module Reloading - 
+        We don't need to restart the app everytime we change the frontend,
+        just saving changes automatically reflects the change.
+        We are doing it with loading URL in the electron app (localhost),
+        instead of the html file in dist-react.
+*/
+
+// npm-run-all : A package used to run multiple npm cmds in parallel.
--- fccProject/vite.config.ts
+++ fccProject/vite.config.ts
@@ -7,5 +7,9 @@ export default defineConfig({
   base: './',                 //changes the default path used in build process
   build: {
     outDir: 'dist-react',     //changed default build directory to stop conflict with electron.
+  },
+  server: {
+    port: 5123,
+    strictPort: true,         //warns if the port is unavailable
   }
 })
